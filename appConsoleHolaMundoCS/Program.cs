a7459d431702c75b571c05550f96cdc1c0814f4d
OSCAR AILTON TORRES MORALES <I21050354@monclova.tecnm.mx>
2021-08-30 03:13:17 -0500

    Agregar archivos de proyecto.

---
+++ appConsoleHolaMundoCS/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appConsoleHolaMundoCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo!!");
+            //Console
+            int a = 367;
+            int b = 117;
+            int c = a + b;
+            Console.WriteLine(a + b);
+
+            Console.ReadKey();
+        }
+    }
+}
